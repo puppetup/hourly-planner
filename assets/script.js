9a8c41f5251346621beefc53c97b3acf0f16f3fc
puppetup <savinohoshizaki@gmail.com>
2022-06-07 19:42:34 -0700

    WHY WONT IT DISPLAY???

--- assets/script.js
+++ assets/script.js
@@ -1,5 +1,4 @@
 
-var scheduleArray = new Array(9)
 var updateMoment = function() {
     var today = moment();
     $('#currentDay').text(today.format('MMMM Do YYYY, h:mm:ss a'));
@@ -10,7 +9,12 @@ setInterval(function(){
     updateMoment()
 }, 1000)
 
+
+kSchedule()
+
 function saveTask() {
+
+    getSchedule()
     var taskTextArea = $(this).siblings('textArea');
     var taskValue = taskTextArea.val();
     var timeId = $(this).siblings('div').attr('id');
@@ -22,12 +26,33 @@ function saveTask() {
      }
     
 
-    scheduleArray.splice((taskObject.time - 9), 1 , taskObject)
+    schedule.splice((taskObject.time - 9), 1 , taskObject)
 
-    localStorage.setItem('schedule', JSON.stringify(scheduleArray));
+    localStorage.setItem('schedule', JSON.stringify(schedule));
     
 
 
+}
+
+var schedule = new Array(9);
+
+function getSchedule() {
+    var storedPairs = localStorage.getItem('schedule');
+    if (storedPairs) {
+        schedule = JSON.parse(storedSchedule);
+    }
+}
+
+function displaySchedule() {
+    getSchedule();
+    for (let i= 0; i < schedule.length; i++) {
+        var scheduleIndex = schedule[i];
+        var textArea = $('#' + (i + 9));
+        if (scheduleIndex) {
+            var x = scheduleIndex.entry;
+            textArea.val(x)
+        }
+    }
 }
 
 $('.saveBtn').on('click', saveTask)
